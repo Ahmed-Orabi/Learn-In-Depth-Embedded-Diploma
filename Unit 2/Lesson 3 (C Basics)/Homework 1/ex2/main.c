92bc7c46b31560c6ac88731c8d71adb9c8bfd2de
Ahmed Gehad Orabi <138608894+Ahmed-Orabi@users.noreply.github.com>
2024-06-24 17:33:44 +0300

    Add files via upload

---
+++ Unit 2/Lesson 3 (C Basics)/Homework 1/ex2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+  Write C Program to Print a Integer Entered by a User
+*/
+int integer;
+int main()
+{
+    printf("Enter a integer: ");
+    scanf("%i",&integer);
+    printf("You entered: %i",integer);
+    return 0;
+}
